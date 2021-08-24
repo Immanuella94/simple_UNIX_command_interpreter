c71391f7d5000a65561a439da5e7765990c99b4f
Immanuella94 <immanuella@gmail.com>
2021-08-24 15:24:25 -0700

    Updated file

--- holberton.h
+++ holberton.h
@@ -1,5 +1,6 @@
 #ifndef SHELL
 #define SHELL
+
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
@@ -8,7 +9,30 @@
 #include <signal.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+
+extern char **environ;
+
+/* Shell Functions */
+char **str_to_2d(char *buff_str, char del[]);
+void exe_new_process(char **args);
+char *status_checker(char **args);
+char *_getenv(const char *name);
+char **cp_environ(void);
+char *_is_ascii(char *buffer);
+
+char *sep_check(char *buffer);
 char *_prompt(char **envp);
 char *_searchpath(char **envi);
 char **_tokpath(char *string);
-#endif
+
+/* Basic Functions */
+int _putchar(char c);
+int p_text(char *text);
+
+/* Strings Functions */
+char *_strdup(char *str);
+int _strcmp(const char *s1, const char *s2);
+char *_strcat(char *s1, char *s2);
+unsigned int _strlen(char *s);
+
+#endif /*SHELL*/
